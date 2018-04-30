5651dfcab7161809ca8acb7af2ca860512b46205
YanC Lin <yonalin1@gmail.com>
2018-04-30 21:24:42 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/' => 'sessions#new'
+ post '/' => 'sessions#create'
+ get '/welcome' => 'sessions#welcome'
+ post '/logout' => 'sessions#destroy'
+ get '/show' => 'secrets#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
